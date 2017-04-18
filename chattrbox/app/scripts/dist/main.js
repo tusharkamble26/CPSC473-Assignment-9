d1bd4c657dc6837b64d8915fd70abea06be35e18
Tushar Kamble <tushark26@csu.fullerton.edu>
2017-04-17 19:31:49 -0700

    Assignment 9

---
+++ chattrbox/app/scripts/dist/main.js
@@ -0,0 +1,29 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ChatApp = function ChatApp() {
+    _classCallCheck(this, ChatApp);
+
+    console.log('Hello ES6!');
+};
+
+exports.default = ChatApp;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var _app = require('./app');
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+new _app2.default();
+
+},{"./app":1}]},{},[2])
